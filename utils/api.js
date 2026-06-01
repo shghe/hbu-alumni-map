63304a4ca9bb4f4eb2f38e969a3522cd1f3aafa0
shghe <china.greatlee@hotmail.com>
2026-06-02 07:46:32 +0800

    v2.0.0: callContainer + app.json cloud:true

--- utils/api.js
+++ utils/api.js
@@ -1,22 +1,17 @@
-const API_BASE = 'https://alumni-api-264838-4-1430752917.sh.run.tcloudbase.com/api'
+const ENV = 'prod-d2gq9dsgy570dcb29'
+const SERVICE = 'alumni-api'
 
 function request(method, path, data) {
   return new Promise((resolve, reject) => {
-    const header = { 'Content-Type': 'application/json' }
-    const token = getApp().globalData.adminToken
-    if (token) header['Authorization'] = `Bearer ${token}`
-
-    wx.request({
-      url: `${API_BASE}${path}`,
+    wx.cloud.callContainer({
+      config: { env: ENV },
+      path,
       method,
-      header,
+      header: { 'X-WX-SERVICE': SERVICE, 'Content-Type': 'application/json' },
       data: method !== 'GET' ? data : undefined,
       success: (res) => {
         if (res.statusCode === 200) resolve(res.data)
-        else if (res.statusCode === 401) {
-          getApp().globalData.adminToken = ''
-          reject(new Error('无权限'))
-        } else reject(new Error((res.data && res.data.message) || '请求失败'))
+        else reject(new Error((res.data && res.data.message) || '请求失败'))
       },
       fail: (err) => reject(err)
     })
@@ -33,4 +28,4 @@ const api = {
   del: (path) => request('DELETE', path)
 }
 
-module.exports = { api, API_BASE }
+module.exports = { api }
