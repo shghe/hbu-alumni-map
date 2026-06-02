7a464b31b184d28eb447f7b42248c3c0b82d7549
shghe <china.greatlee@hotmail.com>
2026-06-02 10:38:34 +0800

    v2.0.0: callContainer 免域名方案

--- utils/api.js
+++ utils/api.js
@@ -1,29 +1,17 @@
-const API_BASE = 'https://alumni-api-264838-4-1430752917.sh.run.tcloudbase.com/api'
-
-// 需要管理员认证的路径前缀
-const ADMIN_PATHS = ['/admin/', '/upload/']
+const ENV = 'prod-d2gq9dsgy570dcb29'
+const SERVICE = 'alumni-api'
 
 function request(method, path, data) {
   return new Promise((resolve, reject) => {
-    const header = { 'Content-Type': 'application/json' }
-
-    // 仅管理员路径附带 token
-    if (ADMIN_PATHS.some(p => path.startsWith(p))) {
-      const token = getApp().globalData.adminToken
-      if (token) header['Authorization'] = `Bearer ${token}`
-    }
-
-    wx.request({
-      url: `${API_BASE}${path}`,
+    wx.cloud.callContainer({
+      config: { env: ENV },
+      path,
       method,
-      header,
+      header: { 'X-WX-SERVICE': SERVICE, 'Content-Type': 'application/json' },
       data: method !== 'GET' ? data : undefined,
       success: (res) => {
         if (res.statusCode === 200) resolve(res.data)
-        else if (res.statusCode === 401) {
-          getApp().globalData.adminToken = ''
-          reject(new Error('无权限'))
-        } else reject(new Error((res.data && res.data.message) || '请求失败'))
+        else reject(new Error((res.data && res.data.message) || '请求失败'))
       },
       fail: (err) => reject(err)
     })
