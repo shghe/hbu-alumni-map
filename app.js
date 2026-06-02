e085ae230d8f285497c61cda2285480028b7644c
shghe <china.greatlee@hotmail.com>
2026-06-02 11:24:37 +0800

    稳定版: wx.request + 不校验域名(开发)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 App({
-  globalData: { appName: 'HBU校友之家地图', adminToken: '' },
-  onLaunch() { wx.cloud.init({ env: 'prod-d2gq9dsgy570dcb29' }) }
+  globalData: {
+    appName: 'HBU校友之家地图',
+    adminToken: ''
+  }
 })
--- utils/api.js
+++ utils/api.js
@@ -1,15 +1,24 @@
-const ENV = 'prod-d2gq9dsgy570dcb29'
+const API_BASE = 'https://alumni-api-264838-4-1430752917.sh.run.tcloudbase.com/api'
+const ADMIN = ['/admin/', '/upload/']
 
 function request(method, path, data) {
   return new Promise((resolve, reject) => {
-    wx.cloud.callContainer({
-      config: { env: ENV },
-      path, method,
-      header: { 'Content-Type': 'application/json' },
+    const h = { 'Content-Type': 'application/json' }
+    if (ADMIN.some(p => path.startsWith(p))) {
+      const t = getApp().globalData.adminToken
+      if (t) h['Authorization'] = `Bearer ${t}`
+    }
+    wx.request({
+      url: API_BASE + path, method, header: h,
       data: method !== 'GET' ? data : undefined,
-      timeout: 15000,
-      success: (res) => resolve(res.data),
-      fail: (err) => reject(err)
+      success: r => {
+        if (r.statusCode === 200) resolve(r.data)
+        else if (r.statusCode === 401) {
+          getApp().globalData.adminToken = ''
+          reject(new Error('无权限'))
+        } else reject(new Error((r.data && r.data.message) || '请求失败'))
+      },
+      fail: reject
     })
   })
 }
@@ -21,7 +30,7 @@ const api = {
   },
   post: (path, data) => request('POST', path, data),
   put: (path, data) => request('PUT', path, data),
-  del: (path) => request('DELETE', path)
+  del: path => request('DELETE', path)
 }
 
 module.exports = { api }
